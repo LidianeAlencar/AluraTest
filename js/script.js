2225f9fa3e80ec98619234a48c4f55af1221586c
LIDIANE ALENCAR MENDONCA <lmendonca@admin.br>
2018-11-14 18:34:42 -0200

    Final

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('teste');
+
+var btn = document.querySelector(".btn-more-info");
+var courseInfo = document.querySelector(".course-information");
+var moreinfo = document.querySelector(".more-info");
+
+btn.addEventListener("click", showInfo);
+courseInfo.style.display = "none";
+moreinfo.style.width = '100%';
+
+
+function showInfo() {	
+
+	if(courseInfo.style.display === "none"){
+		btn.innerHTML = "Menos Informações";
+		courseInfo.style.display = 'flex';
+		moreinfo.style.width = 'inherit';
+
+	}else{
+		courseInfo.style.display = 'none';
+		btn.innerHTML = "Mais Informações";
+		moreinfo.style.width = '100%';
+	}
+
+
+}
